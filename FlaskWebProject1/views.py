f0b9912145a9cffeb3786a56aaf241d842551ef3
hakeemullahjan <hakeemullah.yousufzai@gmail.com>
2026-06-18 10:39:53 +0200

    bump version

--- FlaskWebProject1/views.py
+++ FlaskWebProject1/views.py
@@ -23,7 +23,7 @@ def home():
 def version():
     return jsonify({
         "instance": os.environ.get('INSTANCE_NAME', 'unknown'),
-        "version": "500"
+        "version": "18.0.0",
     })
 @app.route('/contact')
 def contact():
